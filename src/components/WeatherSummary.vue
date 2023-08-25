f3fd2e89a4b08bd062bb85d2e0de157e31c09224
Rekonir <zhed-nikita@mail.ru>
2023-08-25 14:36:49 +0300

    add components + add fetch + sunc WSummary

---
+++ src/components/WeatherSummary.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { capitalizeFirstLetter } from "../utils/index.js";
+
+const props = defineProps({
+  weatherInfo: {
+    type: [Object, null],
+    required: true,
+  },
+});
+const today = new Date().toLocaleString("en-EN", {
+  weekday: "short",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+});
+</script>
+
+<template>
+  <div v-if="weatherInfo?.weather" class="summary">
+    <div
+      :style="`
+        background-image: url('./src/assets/img/weather-main/${weatherInfo?.weather[0].description}.png');
+      `"
+      class="pic-main"
+    ></div>
+    <div class="weather">
+      <div class="temp">{{ Math.round(weatherInfo?.main?.temp) }} °C</div>
+      <div class="weather-desc text-block">
+        {{ capitalizeFirstLetter(weatherInfo?.weather[0].description) }}
+      </div>
+    </div>
+    <div class="city text-block">
+      {{ capitalizeFirstLetter(weatherInfo?.name) }},
+      {{ capitalizeFirstLetter(weatherInfo?.sys?.country) }}
+    </div>
+    <div class="date text-block">{{ today }}</div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "../assets/styles/main.scss";
+</style>
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const API_KEY = '72ead3cd0e28881cd723a09bdd4963b3'
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
